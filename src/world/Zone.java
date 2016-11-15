0d63ceb0d5ba3bb4fd479eb142482b98545de5cd
Luke P <lukeponiatowski@gmail.com>
2016-11-15 13:58:01 -0500

    v 0.3.7co

--- src/world/Zone.java
+++ src/world/Zone.java
@@ -5,16 +5,21 @@
  */
 package world;
 
+import java.awt.Image;
+import java.io.Serializable;
 import java.util.ArrayList;
+import javax.swing.ImageIcon;
 
 /**
  *
  * @author LHP5025
  */
-public class Zone {
+public class Zone implements Serializable {
     private WorldTile[][] zone_tiles; // X, Y
     
     public final String name;
+    public final static WorldTile grass_1 = new FloorTile( new ImageIcon(Class.class.getResource("/rsc/grass_1.png")));
+    public final static WorldTile wall_basic_1 = new FloorTile( new ImageIcon(Class.class.getResource("/rsc/wall_basic_1.png")));
     
     public WorldTile[][] getZone_tiles() {
         return zone_tiles;
